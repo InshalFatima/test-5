a65c6d03f57f3b6ad4cae95b1233cc1a8c8cacbb
InshalFatima <38943108+InshalFatima@users.noreply.github.com>
2018-10-18 17:45:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// const options ={
+//     durations:2000
+// }
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
+
+
+
+
+
+
+  // Or with jQuery
+
+//   $(document).ready(function(){
+//     $('.carousel').carousel();
+//   });
+      
